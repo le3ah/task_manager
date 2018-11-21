e6836bb47757accfaa71167e147f36c493adcaa7
Leah Miller <lkmiller.87@gmail.com>
2018-11-21 16:04:14 -0600

    Adds database refactor in task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,9 +14,13 @@ class Task
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 
-  def self.all
+  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
+    database
+  end
+
+  def self.all
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -24,8 +28,6 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
   end
