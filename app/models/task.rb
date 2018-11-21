aaebda6fe6b8fa7dee15b55094dd6e49dd96e15a
Leah Miller <lkmiller.87@gmail.com>
2018-11-21 16:25:52 -0600

    Adds destroy method

--- app/models/task.rb
+++ app/models/task.rb
@@ -42,4 +42,9 @@ class Task
                       id)
     Task.find(id)
   end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM tasks
+                      WHERE id = ?;", id)
+  end
 end
