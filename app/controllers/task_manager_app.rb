b83f8f3c4eb5274db1186b4c4a48b53bb12d5969
Leah Miller <lkmiller.87@gmail.com>
2018-11-21 14:14:57 -0600

    Add updates to code for use of shotgun

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,7 @@
 class TaskManagerApp < Sinatra::Base
-  set :root, File.expand_path("..", _dir_)
+  set :root, File.expand_path("..", __dir__)
 
   get '/' do
     'hello, world!'
-  end 
+  end
 end
