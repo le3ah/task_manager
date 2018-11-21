c2c8d599847521b73a164c1c410f1daf09c4bb5e
Leah Miller <lkmiller.87@gmail.com>
2018-11-21 15:58:08 -0600

    Adds id method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
+    @id          = task_params["id"] if task_params["id"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
   end
